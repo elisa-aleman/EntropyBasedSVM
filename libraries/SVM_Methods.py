e319ad39db23e1598f5a0a59d11af9de6be7e062
Elisa Claire Alemán Carreón <37046039+elisa-aleman@users.noreply.github.com>
2020-09-13 17:41:01 +0900

    Update SVM_Methods.py

--- libraries/SVM_Methods.py
+++ libraries/SVM_Methods.py
@@ -1,32 +1,34 @@
 #-*- coding: utf-8 -*-
 
-from sklearn import svm
 import scipy
 import numpy
-from libraries.ModelMetrics import *
+from sklearn import svm
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from libraries.Model_metrics import F_score_multiclass_Kfolds, F_score_Kfolds
+from libraries.Bag_of_words import get_feature_names
 
-def Predict(clf, sentence, keyword_list):
-    vectorizer = CountVectorizer(min_df=1, token_pattern='(?u)\\b\\w+\\b') #All words included
-    IM = vectorizer.fit_transform(keyword_list)
-    pre_vector = vectorizer.transform([sentence]).toarray().tolist()
-    vector = numpy.array(pre_vector[0])
-    predicted = clf.predict(vector.reshape(1,-1))[0]
-    return predicted
-
-def SVM_Train(x, y, test_size, shuffle=True, kerne ='linear', C=1.0, gamma=0.001):
-    train_x, test_x, train_y, test_y = train_test_split(x,y, test_size=test_size, shuffle=shuffle)
+def SVM_Train(x, y, test_size, shuffle=True, kernel ='linear', C=1.0, gamma=0.001):
+    if test_size>0:
+        train_x, test_x, train_y, test_y = train_test_split(x,y, test_size=test_size, shuffle=shuffle)
+    else:
+        train_x = x
+        train_y = y
+        test_x = []
+        test_y = []
     testsize = len(test_y)
     #Define classifier
     clf = svm.SVC(kernel = kernel, C = C, gamma = gamma)
     clf.fit(train_x,train_y)
     #Test data
     y_preds = []
-    for i in range(testsize):
-        predicted = clf.predict(test_x[i].reshape(1,-1))[0]
-        y_preds.append(predicted)
+    if test_size>0:
+        for i in range(testsize):
+            predicted = clf.predict(test_x[i].reshape(1,-1))[0]
+            y_preds.append(predicted)
     return clf, test_y, y_preds
 
-def SVM_Kfolds(x, y, k, kernel='linear', C=1.0, gamma=0.001):
+def SVM_Kfolds(x, y, k, kernel='linear', C=1.0, gamma=0.001, multiclass=False, with_counts=True, with_lists=True, with_confusion_matrix=True):
     test_size = len(y)//k
     y_pred_list = []
     true_ys_list = []
@@ -34,8 +36,11 @@ def SVM_Kfolds(x, y, k, kernel='linear', C=1.0, gamma=0.001):
         clf, test_y, y_preds = SVM_Train(x, y, test_size, shuffle=True, kernel=kernel, C=C, gamma=gamma)
         y_pred_list.append(y_preds)
         true_ys_list.append(test_y)
-    results = F_score_Kfolds(true_ys_list, y_pred_list)
-    return clf, results
+    if multiclass:
+        results = F_score_multiclass_Kfolds(true_ys_list, y_pred_list, with_counts=with_counts, with_lists=with_lists, with_confusion_matrix=with_confusion_matrix)
+    else:
+        results = F_score_Kfolds(true_ys_list, y_pred_list, with_counts=with_counts, with_lists=with_lists)
+    return results
 
 def SVM_weights_untrained(x, y, feature_names, kernel = 'linear', C = 1.0, gamma = 0.001):
     if type(x) == type([]):
@@ -50,7 +55,8 @@ def SVM_weights_untrained(x, y, feature_names, kernel = 'linear', C = 1.0, gamma
 
 def SVM_weights_trained(clf,keyword_list):
     weights = clf.coef_.tolist()[0]
-    influences = list(zip(keyword_list, weights))
+    feature_names = get_feature_names(keyword_list)
+    influences = list(zip(feature_names, weights))
     return influences
 
 if __name__ == "__main__":
