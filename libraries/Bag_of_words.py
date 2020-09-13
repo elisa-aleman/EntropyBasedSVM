93c14fd36201921604829974f9ce3ed2baa1ad98
Elisa Claire Alemán Carreón <37046039+elisa-aleman@users.noreply.github.com>
2020-09-13 17:16:36 +0900

    Update and rename BagOfWords.py to Bag_of_words.py

--- libraries/Bag_of_words.py
+++ libraries/Bag_of_words.py
@@ -1,25 +1,30 @@
 #-*- coding: utf-8 -*-
 
 from sklearn.feature_extraction.text import CountVectorizer
+import numpy
 
-def Vectorize_Bag_of_Words(sentences, word_list):
-    # sentences: ["text",1 or 0] 1: positive, 0: negative
-    # word_list = ['word', 'word', 'word']
-    corpus = [sentence[0] for sentence in sentences]
-    y_list = [sentence[1] for sentence in sentences]
+def Vectorize_training_data_Bag_of_Words(training_sentences, word_list):
+    '''
+    training_sentences: ["text",1 or 0] 1: positive, 0: negative
+    word_list = ['word', 'word', 'word']
+    '''
+    corpus = [sentence[0] for sentence in training_sentences]
+    y_list = [sentence[1] for sentence in training_sentences]
     X = Bag_of_Words(corpus, word_list)
     y = numpy.array(y_list)
     return X, y
 
 def Bag_of_Words(corpus, word_list):
-    # corpus: ["text1", "text2", ...]
-    # word_list = ['word', 'word', 'word']
+    '''
+    corpus: ["text1", "text2", ...]
+    word_list = ['word', 'word', 'word']
+    '''
     vectorizer = CountVectorizer(min_df=1, token_pattern='(?u)\\b\\w+\\b')
     IM = vectorizer.fit_transform(word_list)
     X = vectorizer.transform(corpus).toarray()
     return X
 
-def Feature_Names(keyword_list):
+def get_feature_names(keyword_list):
     vectorizer = CountVectorizer(min_df=1, token_pattern='(?u)\\b\\w+\\b')
     IM = vectorizer.fit_transform(keyword_list)
     features = vectorizer.get_feature_names()
