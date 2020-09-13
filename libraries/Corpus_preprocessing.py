892b8311425bfa23b8e45486365576ad54d40a4f
Elisa Claire Alemán Carreón <37046039+elisa-aleman@users.noreply.github.com>
2020-09-13 17:19:53 +0900

    Update Corpus_preprocessing.py

--- libraries/Corpus_preprocessing.py
+++ libraries/Corpus_preprocessing.py
@@ -1,6 +1,7 @@
 #-*- coding: utf-8 -*-
 
 import gensim
+# from gensim.summarization.textcleaner import split_sentences
 
 def LemmatizeEnglish(content):
     lem = ' '.join([i.decode('utf-8').split('/')[0] for i in gensim.utils.lemmatize(content)])
@@ -23,7 +24,6 @@ def Dictionary(tokenized):
     # dictionary.save(MakeLogFile("gensimdictionary.dict"))
     return dictionary
 
-
 def general_dictionary(corpus):
     '''
     Input: corpus --> list of space separated documents of ALL categories
@@ -33,6 +33,10 @@ def general_dictionary(corpus):
     dictionary = Dictionary(tokenized)
     general_dict = sorted([word for word in dictionary.values()])
     return general_dict
+
+# You don't need to import this one, but I put it here for reference
+# def wrapper_split_sentences(text):
+#     split_sentences(text)
     
 if __name__ == '__main__':
     pass
